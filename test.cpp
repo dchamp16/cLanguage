4873471a1221d5bcc8210141371e9b0b10b344c9
Peter Ramos <dchamp16@gmail.com>
2023-06-01 07:32:18 -0600

    string

--- test.cpp
+++ test.cpp
@@ -1,25 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    int x = 5; // 0101
-    x ^= 3;    // 0011
+    string fname, lname, fullName;
+    fname = "Peter ";
+    lname = "Ramos";
+    fullName = fname + lname;
+    cout << fullName;
 
-    /*
-    if it comparing 0 to 0 = 0
-    if its 1 to 1 = 0
-    if its 1 to 0 = 1
-
-    0 1 0 1 = 5
-    ^ ^ ^ ^
-    0 0 1 1 = 3
-    = = = =
-    0 1 1 0 = 6
-
-
-    */
-
-    cout << x; // 0 1 1 0 = 6
     return 0;
 }
