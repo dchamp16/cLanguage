7344047cecc673e6e8f9afe571afe3a269fa043e
Peter Ramos <dchamp16@gmail.com>
2023-06-06 12:42:54 -0600

    reference

--- test.cpp
+++ test.cpp
@@ -4,22 +4,19 @@ using namespace std;
 
 int main()
 {
-    struct
-    {
-        int num1, num2, num3;
-        string firstName, lastName, middleName;
-        bool isMale;
-    } person;
+    // reference
+    int original = 10;
+    int &ref = original;
+    ref = 33;
+    original = 40;
 
-    person.num1 = 22;
-    person.num2 = 10;
-    person.num3 = 33;
-    person.firstName = "Peter ";
-    person.firstName = "Ramos ";
-    person.middleName = "Guevara";
-    person.isMale = true;
-
-    cout << "Fullname: " << person.firstName << person.middleName << person.firstName << "Age: " << person.num3 << (person.isMale ? " Male" : " Female");
+    cout << "original: " << original << " ref: " << ref << "\n";
+    // ----------------
+    string fName, lName;
+    fName = "Peter";
+    lName = "Ramos";
+    cout << "fName Address: " << &fName << " | "
+         << "lName Address: " << &lName;
 
     return 0;
 }
