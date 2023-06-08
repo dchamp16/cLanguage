ad9a06e75664534ea7172f4f72faac8cd4bbc7ab
Peter Ramos <dchamp16@gmail.com>
2023-06-08 13:18:15 -0600

    update

--- test.cpp
+++ test.cpp
@@ -2,32 +2,22 @@
 #include <string>
 using namespace std;
 
-/* return a added integer */
-int plusFunc(int x, int y)
+int factorial(int n)
 {
-    return x + y;
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n - 1);
+    }
 }
 
-/* return a added double */
-double plusFunc(double x, double y)
-{
-    return x + y;
-}
-
-/* return a concatenate string */
-string plusFunc(string x, string y)
-{
-    return x + y;
-}
-
-/* MAIN */
 int main()
 {
-    int myNum1 = plusFunc(8, 5);
-    double myNum2 = plusFunc(4.3, 6.26);
-    string myNum3 = plusFunc("6", "30");
-    cout << "Int: " << myNum1 << "\n";
-    cout << "Double: " << myNum2 << "\n";
-    cout << "String: " << myNum3;
+    int number = 5;
+    int result = factorial(number);
+    cout << "Factorial of " << number << " is: " << result << endl;
     return 0;
 }
