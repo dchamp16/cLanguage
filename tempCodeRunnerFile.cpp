06634d358f024d30dabd74569b92da5f313fcbec
Peter Ramos <dchamp16@gmail.com>
2023-06-20 14:43:24 -0600

    continue

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+int foo(int &num)
+{
+    int temp = num;
+    num = 70;
+    return num;
+}
--- test.cpp
+++ test.cpp
@@ -3,18 +3,29 @@
 
 int foo(int &num)
 {
-    return num = 70;
+    num = 70;
+    return num;
 }
+class Calculator
+{
+public:
+    int num1, num2;
+    Calculator(int &prob1, int &prob2) : num1(prob1), num2(prob2) {}
+    int add()
+    {
+        return this->num1 + this->num2;
+    }
+};
 
 int main()
 {
-    int num = 32;
-    int &ref = num;
-    int *ptr = &ref;
-    ref = 100;
-    num = 9999;
-    foo(ref);
+    Calculator prob1(1, 2);
+    std::cout << prob1.add() << "\n";
 
-    std::cout << num << "\t" << ref << "\t" << ptr;
+    int num = 32;
+    const int &ref = num;
+
+    std::cout << num << "\t" << foo(num) << "\n";
+    std::cout << num << "\t" << ref;
     return 0;
 }
